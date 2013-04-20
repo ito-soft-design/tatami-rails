ffeee282d7fc6c6b95d1fe9367d1451fc06572a3
Katsuyoshi Ito <kito@itosoft.com>
2013-04-21 08:44:56 +0900

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tatami::Application.config.secret_token = 'fda1dbb36b6ce4a30c5f232c3324862efe4661309ece2cbfd9472e94cd57e726fe99c1c47b6e3e71add2ae65db45b9f60cb6b0680c88d63f432916f75ce2c32c'
