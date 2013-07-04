3a43467f0c24fac89c28b08c19613b4a821f1d97
Katsuyoshi Ito <kito@itosoft.com>
2013-07-04 11:56:20 +0900

    add japanese description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :get_domain
 
+  before_filter :set_locale
+
 =begin
   before_filter :password_protected if Rails.env.production?
 
@@ -14,8 +16,26 @@ class ApplicationController < ActionController::Base
   end
 =end
 
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
+  def set_locale
+    if params[:locale].nil?
+      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+      I18n.locale = extract_locale_from_accept_language_header
+      logger.debug "* Locale set to '#{I18n.locale}'"
+    else
+      I18n.locale = params[:locale]
+    end
+  end
+
   private
   
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first 
+  end
+
   def get_domain
     $http_host = request.env['HTTP_HOST']
     $http_schema = request.env['rack.url_scheme']
